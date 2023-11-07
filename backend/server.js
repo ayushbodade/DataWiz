f1158d55208a07ca4c2fa15ebd94137d19071fdb
Utsav <utsav.avaiya@spit.ac.in>
2023-11-08 02:35:25 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ PORT = 8080;
 const app = express();
 app.use(fileUpload({ useTempFiles: true }));
 const corsOpts = {
-  origin: "*",
+  origin: "https://data-wiz-client.vercel.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200
 };
