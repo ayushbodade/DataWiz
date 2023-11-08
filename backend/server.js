40295e4398a6d8a369e8f8e00f811a85820b9682
Utsav <utsav.avaiya@spit.ac.in>
2023-11-08 10:18:38 +0530

    Vercel Implementation

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,10 @@ app.use(express.json());
 const userRoutes = require("./routes/user");
 app.use("/user", userRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
 app.listen(PORT, () => {
   console.log("Server running on port ", PORT);
   connectDatabase();
